5b7edd6bee0f6fa661f0cbbf892d494503f386a4
Mohamed Abdelgwad <4671486+scr2em@users.noreply.github.com>
2024-10-15 15:11:53 +0300

    fix: add more methods for testing in the UI

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,10 +1,17 @@
 import { LLScanner } from 'll-scanner';
 
-window.testEcho = () => {
-  LLScanner.startScanning();
+window.startFrontScanning = () => {
+  LLScanner.startScanning({ cameraDirection: 'FRONT' });
 };
 
+window.startBackScanning = () => {
+  LLScanner.startScanning({ cameraDirection: 'BACK' });
+};
+window.stopScanning = () => {
+  LLScanner.stopScanning();
+};
 const set = new Set();
+
 LLScanner.addListener('barcodesScanned', (e) => {
   const scannedCode = e.scannedCode;
   if (set.has(scannedCode)) {
