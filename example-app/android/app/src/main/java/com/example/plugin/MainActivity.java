177f80fa343bd7bdefdeea7ecfb30c91ea9e4cc4
Amr Abdelmagid <amr.abdelmagid@leadliaison.com>
2024-10-18 17:50:04 +0300

    Register LLScannerPlugin in Capacitor for android example project

--- example-app/android/app/src/main/java/com/example/plugin/MainActivity.java
+++ example-app/android/app/src/main/java/com/example/plugin/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.plugin;
-
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.leadliaion.llscanner.LLScannerPlugin;
 
-public class MainActivity extends BridgeActivity {}
+@androidx.camera.core.ExperimentalGetImage
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(LLScannerPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
