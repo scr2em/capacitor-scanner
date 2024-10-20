312f5e97f12625f56d274797f28e87efac1c664f
Mohamed Abdelgwad <4671486+scr2em@users.noreply.github.com>
2024-10-20 13:36:53 +0300

    feat: rename to capacitor-scanner

--- example-app/android/app/src/main/java/com/example/plugin/MainActivity.java
+++ example-app/android/app/src/main/java/com/example/plugin/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.plugin;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.leadliaion.llscanner.LLScannerPlugin;
+import com.leadliaion.capacitorscanner.CapacitorScannerPlugin;
 
 @androidx.camera.core.ExperimentalGetImage
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(LLScannerPlugin.class);
+    registerPlugin(CapacitorScannerPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
